cff56d333c9adc16565cdc2c1398366262455fcb
Neetu <93900988+Neetu-12@users.noreply.github.com>
2022-05-29 22:40:19 +0530

    Update delete.js

--- CRUD/delete.js
+++ CRUD/delete.js
@@ -1,6 +1,6 @@
 const req = require('express');
 var mysql = require('mysql')
-const fs = require('fs')
+// const fs = require('fs')
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -9,24 +9,26 @@ var connection = mysql.createConnection({
 })
 connection.connect();
 
-// connection.query('insert students values(1,"Neetu","sahneetu754@gmail.com")', (err, rows) => {
-//     if (err) throw err
-//     var data = JSON.stringify(rows, null, 4)
-//     fs.writeFileSync("delete.json",data)
-//     console.log(data);
-// })
 
-connection.query('select * from students', (err, rows) => {
-    if (err) throw err
-    var data = JSON.stringify(rows, null, 4)
-    fs.writeFileSync("detele.JSON",data)
+app.delete("/home",(req,res)=>{
+    var id=req.body.id
+    var name=req.body.name
+    var marks=req.body.marks
+    sql.query(`delete from students where id=${id}`,(err,data)=>{
+        if(data){
+            res.send("deleted")
+        }
+        else{
+            res.send(err)
+        }
+      
+    })
 
-    console.log(data);
 })
 
-connection.query('delete from students where id=1',(err,rows)=>{
-    if(err) throw err
-    data=JSON.stringify(rows,null,4)
-    fs.writeFileSync(data)
+
+app.listen(3000, () => {
+    console.log("hiii");
 })
-connection.end()
+
+
